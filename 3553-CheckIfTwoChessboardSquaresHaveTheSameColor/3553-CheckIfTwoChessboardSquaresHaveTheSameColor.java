e91e0d1bc4f196b078cb514e5f07155e20110643
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-13 23:29:51 +0530

    Create 3553-CheckIfTwoChessboardSquaresHaveTheSameColor

---
+++ 3553-CheckIfTwoChessboardSquaresHaveTheSameColor/3553-CheckIfTwoChessboardSquaresHaveTheSameColor.java
@@ -0,0 +1,8 @@
+// Last updated: 10/13/2025, 11:29:50 PM
+public class Solution {
+    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+        int one = (coordinate1.charAt(0) - 'a') + (coordinate1.charAt(1) - '0');
+        int two = (coordinate2.charAt(0) - 'a') + (coordinate2.charAt(1) - '0');
+        return one % 2 == two % 2;
+    }
+}
