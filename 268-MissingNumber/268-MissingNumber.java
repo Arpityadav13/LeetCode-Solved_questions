bf371a54a547977cbbf2fd798fcb1be17f3c91d6
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-13 23:31:18 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 10/13/2025, 11:31:18 PM
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i =0;i<nums.length+1;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
