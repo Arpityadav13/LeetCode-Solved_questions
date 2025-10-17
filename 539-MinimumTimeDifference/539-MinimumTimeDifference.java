3bdb7cf822d1eb61c988fd0be60049e174c1319a
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-17 21:13:49 +0530

    Create 539-MinimumTimeDifference

---
+++ 539-MinimumTimeDifference/539-MinimumTimeDifference.java
@@ -0,0 +1,25 @@
+// Last updated: 10/17/2025, 9:13:47 PM
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans =Integer.MAX_VALUE;
+        
+
+    for(int i =0 ; i<nums.length-2;i++){
+        int left = i+1;
+        int right = nums.length-1;
+        while(left < right){
+            int sum = nums[i]+nums[left]+nums[right];
+            
+            if(Math.abs(target - sum)< Math.abs(target - ans)) ans = sum;
+
+            else if (sum<target){
+                left++;}
+
+            else{
+            right--;}
+        }
+       
+    }
+     return ans;
+}}
