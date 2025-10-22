298aa65fdf403175e953a6a66d3a34bb5ddee6ee
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-22 21:07:15 +0530

    Create 2469-LongestSubsequenceWithLimitedSum

---
+++ 2469-LongestSubsequenceWithLimitedSum/2469-LongestSubsequenceWithLimitedSum.java
@@ -0,0 +1,24 @@
+// Last updated: 10/22/2025, 9:07:14 PM
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int[] k = new int[queries.length];
+        for(int j = 0;j<queries.length;j++){
+            int sum = 0;
+            int count = 0;
+            for(int i = 0;i<nums.length;i++){
+            if(sum + nums[i] > queries[j]){
+                break;
+            }
+            sum+=nums[i];
+            count++;
+           
+            
+
+        }
+        k[j]=count;
+        }
+        return k;
+        
+    }
+}
