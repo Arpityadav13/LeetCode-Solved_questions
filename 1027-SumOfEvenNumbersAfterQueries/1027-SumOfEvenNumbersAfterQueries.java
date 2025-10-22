b518f45ff2e98ab367759e60aac39c63a5c0e43c
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-22 21:07:35 +0530

    Create 1027-SumOfEvenNumbersAfterQueries

---
+++ 1027-SumOfEvenNumbersAfterQueries/1027-SumOfEvenNumbersAfterQueries.java
@@ -0,0 +1,18 @@
+// Last updated: 10/22/2025, 9:07:34 PM
+class Solution {
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        int[] answers = new int[queries.length];
+        for(int i = 0; i<queries.length;i++){
+            
+            nums[queries[i][1]] = nums[queries[i][1]] + queries[i][0];
+            int sum = 0;
+            for(int j =0 ;j<nums.length;j++){
+                if(nums[j]%2==0){
+                    sum+=nums[j];
+                }
+            }
+            answers[i]=sum;
+        }
+        return answers;
+    }
+}
