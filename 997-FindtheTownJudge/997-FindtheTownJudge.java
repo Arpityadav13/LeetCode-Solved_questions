668918f3f60fd62ebb539d91b87553b38173c869
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-25 22:28:24 +0530

    Create 997-FindtheTownJudge

---
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -0,0 +1,19 @@
+// Last updated: 10/25/2025, 10:28:23 PM
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n==1) return 1;
+        int[] in = new int[n+1];
+        int[] out = new int[n+1];
+        for(int i=0 ;i<trust.length;i++){
+            in[trust[i][1]]++;
+            out[trust[i][0]]++;
+
+        }
+        for(int i =0;i<=n;i++){
+            if(in[i] == (n-1) && out[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
