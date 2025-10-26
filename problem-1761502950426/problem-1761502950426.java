1c6cd7e2689fbff1936ec5040192fe718da0cb41
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-10-26 23:52:31 +0530

    Create

---
+++ problem-1761502950426/problem-1761502950426.java
@@ -0,0 +1,44 @@
+// Last updated: 10/26/2025, 11:52:30 PM
+import java.util.*;
+
+class Bank {
+    private long[] balances;
+    private int n;
+
+    public Bank(long[] balance) {
+        this.balances = balance;
+        this.n = balance.length;
+    }
+
+    private boolean isValidAccount(int account) {
+        return account >= 1 && account <= n;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (!isValidAccount(account1) || !isValidAccount(account2)) {
+            return false;
+        }
+        int i1 = account1 - 1;
+        int i2 = account2 - 1;
+        if (balances[i1] < money) return false;
+
+        balances[i1] -= money;
+        balances[i2] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (!isValidAccount(account)) return false;
+        balances[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (!isValidAccount(account)) return false;
+        int idx = account - 1;
+        if (balances[idx] < money) return false;
+        balances[idx] -= money;
+        return true;
+    }
+}
+
