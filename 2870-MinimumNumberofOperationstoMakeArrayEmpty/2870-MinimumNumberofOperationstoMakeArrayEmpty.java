85258935be2875e0b52c7147cb41487169f4aa10
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-12-22 14:30:42 +0530

    File updated at 12/22/2025, 2:30:43 PM

--- 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.java
+++ 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.java
@@ -1,4 +1,4 @@
-// Last updated: 12/22/2025, 2:30:29 PM
+// Last updated: 12/22/2025, 2:30:43 PM
 1class Solution {
 2    public int minOperations(int[] nums) {
 3        int output = 0;
@@ -6,22 +6,19 @@
 5        for(int num : nums){
 6            map.put(num,map.getOrDefault(num,0)+1);
 7        }
-8        for (int key : map.keySet()) {
-9    System.out.println("Key: " + key + ", Value: " + map.get(key));
-10}
-11        for(int key : map.keySet()){
-12            int feq =map.get(key);
-13            if(feq==1){
-14                return -1;
-15            }
-16            if(feq%3==0){
-17                output+=feq/3;
-18            }else if(feq%3==1) {
-19                output+=(feq-4)/3+2;
-20            }else{
-21                output+=feq/3+1;
-22            }
-23        }
-24        return output;
-25    }
-26}
+8        for(int key : map.keySet()){
+9            int feq =map.get(key);
+10            if(feq==1){
+11                return -1;
+12            }
+13            if(feq%3==0){
+14                output+=feq/3;
+15            }else if(feq%3==1) {
+16                output+=(feq-4)/3+2;
+17            }else{
+18                output+=feq/3+1;
+19            }
+20        }
+21        return output;
+22    }
+23}
