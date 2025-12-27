e788a3cb30c71c3e3512f5b78cea9ad54181ab50
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-12-27 23:12:58 +0530

    File updated at 12/27/2025, 11:12:58 PM

--- 645-SetMismatch/645-SetMismatch.java
+++ 645-SetMismatch/645-SetMismatch.java
@@ -1,21 +1,21 @@
-// Last updated: 12/27/2025, 10:50:23 PM
+// Last updated: 12/27/2025, 11:12:58 PM
 1class Solution {
-2    public int[] findErrorNums(int[] nums) {
-3        HashSet<Integer> set =new HashSet<>();
-4        int[] l =new int[2];
-5        for(int i = 0;i<nums.length;i++){
-6            if(set.contains(nums[i])){
-7                l[0]=nums[i];
-8            }
-9            set.add(nums[i]);
-10        }for(int i = 1;i<=nums.length;i++){
-11                        if (!set.contains(i)) {
-12                l[1] = i;
-13                break;
+2    public List<Integer> findDisappearedNumbers(int[] nums) {
+3        List<Integer> result = new ArrayList<>();
+4        HashSet<Integer> set = new HashSet<>();
+5
+6        for (int num : nums) {
+7            set.add(num);
+8        }
+9
+10        int n = nums.length;
+11        for (int i = 1; i <= n; i++) {
+12            if (!set.contains(i)) {
+13                result.add(i);
 14            }
-15
-16        }
-17
-18        return l;
-19    }
-20}
+15        }
+16
+17        return result;
+18    }
+19}
+20
