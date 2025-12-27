25f11fae0e0e5d82765157f5aba0eee14161a9fd
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2025-12-27 22:50:23 +0530

    File updated at 12/27/2025, 10:50:23 PM

--- 645-SetMismatch/645-SetMismatch.java
+++ 645-SetMismatch/645-SetMismatch.java
@@ -1,18 +1,21 @@
-// Last updated: 12/27/2025, 10:49:01 PM
+// Last updated: 12/27/2025, 10:50:23 PM
 1class Solution {
-2    public int[] smallerNumbersThanCurrent(int[] nums) {
-3        int[] output = new int[nums.length];
-4        for(int i = 0;i<nums.length;i++){
-5            for(int j = 0;j<nums.length;j++){
-6                if(j==i){
-7                    continue;
-8                }
-9                if(nums[j]<nums[i]){
-10                    output[i]++;
-11                }
-12
-13            }
-14        }
-15        return output;
-16    }
-17}
+2    public int[] findErrorNums(int[] nums) {
+3        HashSet<Integer> set =new HashSet<>();
+4        int[] l =new int[2];
+5        for(int i = 0;i<nums.length;i++){
+6            if(set.contains(nums[i])){
+7                l[0]=nums[i];
+8            }
+9            set.add(nums[i]);
+10        }for(int i = 1;i<=nums.length;i++){
+11                        if (!set.contains(i)) {
+12                l[1] = i;
+13                break;
+14            }
+15
+16        }
+17
+18        return l;
+19    }
+20}
