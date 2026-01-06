82550c1c0ded1bd462af1f970fc124e926d44746
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-01-06 09:18:48 +0530

    File updated at 1/6/2026, 9:18:47 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,25 +1,25 @@
-// Last updated: 10/13/2025, 11:31:47 PM
-class Solution {
-    public int evalRPN(String[] tokens) {
-        Stack<Integer> s = new Stack<>();
-        int ans = 0;
-        for(int i = 0 ; i< tokens.length;i++){
-            if(tokens[i].equals("+")){
-                s.push(s.pop()+s.pop());
-                }
-            else if(tokens[i].equals("-")){
-                int minus =  s.pop();
-                s.push(s.pop()-minus);
-            }else if(tokens[i].equals("*")){
-                s.push(s.pop()*s.pop());
-            }else if(tokens[i].equals("/")){
-                int minus =  s.pop();
-                s.push(s.pop()/minus);
-            }else{
-                s.push(Integer.valueOf(tokens[i]));
-            }
-            
-        }
-        return s.peek();
-    }
-}
+// Last updated: 1/6/2026, 9:18:47 AM
+1class Solution {
+2    public int evalRPN(String[] tokens) {
+3        Stack<Integer> s = new Stack<>();
+4        int ans = 0;
+5        for(int i = 0 ; i< tokens.length;i++){
+6            if(tokens[i].equals("+")){
+7                s.push(s.pop()+s.pop());
+8                }
+9            else if(tokens[i].equals("-")){
+10                int minus =  s.pop();
+11                s.push(s.pop()-minus);
+12            }else if(tokens[i].equals("*")){
+13                s.push(s.pop()*s.pop());
+14            }else if(tokens[i].equals("/")){
+15                int minus =  s.pop();
+16                s.push(s.pop()/minus);
+17            }else{
+18                s.push(Integer.valueOf(tokens[i]));
+19            }
+20            
+21        }
+22        return s.peek();
+23    }
+24}
