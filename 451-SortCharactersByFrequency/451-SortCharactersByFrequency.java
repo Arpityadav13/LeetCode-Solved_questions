337670b6ee266cd094625d4740e4e14241f3baf3
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-01-19 21:56:14 +0530

    File updated at 1/19/2026, 9:56:14 PM

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -1,20 +1,18 @@
-// Last updated: 1/19/2026, 9:20:53 PM
+// Last updated: 1/19/2026, 9:56:14 PM
 1class Solution {
-2    public String frequencySort(String s) {
-3        HashMap<Character,Integer> map = new HashMap<>();
-4        for(char c : s.toCharArray()){
-5            map.put(c,map.getOrDefault(c,0)+1);
-6        }
-7        PriorityQueue<Character> queue = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
-8        queue.addAll(map.keySet());
-9        StringBuilder sb =new StringBuilder();
-10        while(!queue.isEmpty()){
-11            char c = queue.poll();
-12            int count = map.get(c);
-13            for(int i = 0;i<count;i++){
-14                sb.append(c);
-15            }
-16
-17        }
-18        return sb.toString();}
-19}
+2    public int minStoneSum(int[] piles, int k) {
+3        PriorityQueue<Integer> pq =new PriorityQueue<>((a,b)->b-a);
+4        for(int pile : piles) pq.offer(pile);
+5        while(k>0){
+6            int var = pq.poll();
+7            pq.add(var-(int)Math.floor(var/2));
+8            k--;
+9        }
+10        int sum =0;
+11        while(!pq.isEmpty()){
+12            sum+=pq.poll();
+13        }
+14        
+15        return sum;
+16    }
+17}
