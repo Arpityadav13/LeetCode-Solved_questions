3a44715e43f65300addd3f1f6afced8368642989
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-01-27 00:52:04 +0530

    Create 1200-MinimumAbsoluteDifference

---
+++ 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.java
@@ -0,0 +1,17 @@
+// Last updated: 1/27/2026, 12:52:02 AM
+1class Solution {
+2    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+3        Arrays.sort(arr);
+4        int minDiff = Integer.MAX_VALUE;
+5        for(int i = 0; i < arr.length - 1; i ++) {
+6            minDiff = Math.min(minDiff, arr[i + 1] - arr[i]);
+7        }
+8        List<List<Integer>> res = new ArrayList<>();
+9        for(int i = 0; i < arr.length - 1; i ++){
+10            if (arr[i + 1] - arr[i] == minDiff) {
+11                res.add(Arrays.asList(arr[i], arr[i + 1]));
+12            }
+13        }
+14        return res;
+15    }
+16}
