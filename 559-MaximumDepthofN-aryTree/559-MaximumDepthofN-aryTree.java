0769a9df260892e226b7aed65ebc35fb58260822
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-04-16 11:32:48 +0530

    Create 559-MaximumDepthofN-aryTree

---
+++ 559-MaximumDepthofN-aryTree/559-MaximumDepthofN-aryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 4/16/2026, 11:32:46 AM
+1/*
+2// Definition for a Node.
+3class Node {
+4    public int val;
+5    public List<Node> children;
+6
+7    public Node() {}
+8
+9    public Node(int _val) {
+10        val = _val;
+11    }
+12
+13    public Node(int _val, List<Node> _children) {
+14        val = _val;
+15        children = _children;
+16    }
+17};
+18*/
+19
+20class Solution {
+21    public int maxDepth(Node root) {
+22       if(root == null ) return 0;
+23       int max =0 ;
+24       for(Node child : root.children){
+25        max= Math.max(max,maxDepth(child));
+26       }
+27       return max+1;
+28    }
+29}
