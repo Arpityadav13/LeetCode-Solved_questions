ea6e3365fcfe9d16c6a2b557387dceb3ff429d06
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-04-22 16:49:39 +0530

    File updated at 4/22/2026, 4:49:38 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,24 +1,20 @@
-// Last updated: 10/13/2025, 11:31:21 PM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i= 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        for(int i =0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch)==1){
-                    map.remove(ch);
-                }else{
-                    map.put(ch,map.get(ch)-1);
-                }
-
-            }else{
-                return false;
-            }
-        }
-        return map.isEmpty();
-    }
-}
+// Last updated: 4/22/2026, 4:49:38 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if(s.length() != t.length()) return false;
+4        int[] arr1 = new int[26];
+5        int[] arr2 = new int[26];
+6        for(int i = 0;i<s.length();i++){
+7            arr1[s.charAt(i)-'a']++;
+8            arr2[t.charAt(i)-'a']++;
+9        }
+10        for(int i = 0;i<26;i++){
+11            if(arr1[i] != arr2[i]){
+12                return false;
+13            }
+14        }
+15        return true;
+16
+17
+18    }
+19}
