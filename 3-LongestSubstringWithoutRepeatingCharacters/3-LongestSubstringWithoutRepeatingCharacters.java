f58181c6b604911525a92ad8f86704d3366a0044
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-04-26 01:16:23 +0530

    File updated at 4/26/2026, 1:16:22 AM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,16 +1,17 @@
-// Last updated: 10/31/2025, 2:25:31 PM
-class Solution {
-    public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int output =0 ;
-        for(int i = 0;i<costs.length;i++){
-            if(costs[i]<=coins){
-                output++;
-                coins-=costs[i];
-            }else{
-                 break;
-            }
-        }
-        return output;
-    }
-}
+// Last updated: 4/26/2026, 1:16:22 AM
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        HashMap<Character,Integer> map = new HashMap<>();
+4        int maxlen= 0 ;
+5        int left = 0;
+6        for(int right = 0;right<s.length();right++){
+7            char c = s.charAt(right);
+8            if(map.containsKey(c) && map.get(c) >= left){
+9                left = map.get(c)+1;
+10            }
+11        map.put(c,right);
+12        maxlen=Math.max(maxlen,right-left+1);
+13        }
+14        return maxlen;
+15    }
+16}
