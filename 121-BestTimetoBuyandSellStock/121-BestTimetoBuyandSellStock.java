f5aa5e5216ab03fa02447489f9f70ffb9238a5a3
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-05-08 17:59:26 +0530

    File updated at 5/8/2026, 5:59:25 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,25 +1,21 @@
-// Last updated: 5/8/2026, 5:37:58 PM
+// Last updated: 5/8/2026, 5:59:25 PM
 1class Solution {
-2    public int[][] merge(int[][] intervals) {
-3        Arrays.sort(intervals,Comparator.comparingInt(a->a[0]));
-4        List<int[]> list = new ArrayList<>();
-5        int[] prev = new int[2];
-6        prev[0]=intervals[0][0];
-7        prev[1]=intervals[0][1];
-8        for(int i =1;i<intervals.length;i++){
-9            int curr0 = intervals[i][0];
-10            int curr1 = intervals[i][1];
-11            if(prev[1]>=curr0){
-12                prev[0] =Math.min(prev[0],curr0);
-13                prev[1] =Math.max(prev[1],curr1);
-14            }
-15            else{
-16                list.add(prev);
-17                prev = intervals[i];
-18            }
-19
-20        }
-21        list.add(prev);
-22        return list.toArray(new int[list.size()][]);
-23    }}
-24
+2    public int[] productExceptSelf(int[] nums) {
+3        int[] prefix = new int[nums.length];
+4        int[] surfix = new int[nums.length];
+5        prefix[0] = 1;
+6        surfix[nums.length-1] = 1;
+7        for(int i = 1;i<nums.length;i++){
+8            prefix[i]=prefix[i-1]*nums[i-1];
+9        }
+10        for(int j =nums.length-2;j>=0;j--){
+11             surfix[j]=surfix[j+1]*nums[j+1];
+12        }
+13        int[] result = new int[nums.length];
+14        for(int i =0;i<nums.length;i++){
+15            result[i] = surfix[i]*prefix[i];
+16        }
+17        return result;
+18        
+19    }
+20}
