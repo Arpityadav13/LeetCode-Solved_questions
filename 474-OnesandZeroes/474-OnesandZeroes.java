519a9fd8d003bc99c558413f50e384df977ada49
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-05-26 21:25:24 +0530

    File updated at 5/26/2026, 9:25:24 PM

--- 474-OnesandZeroes/474-OnesandZeroes.java
+++ 474-OnesandZeroes/474-OnesandZeroes.java
@@ -1,16 +1,35 @@
-// Last updated: 11/11/2025, 11:16:18 PM
-class Solution {
-    public int findMaxForm(String[] S, int M, int N) {
-        int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray())
-                if (c == '0') zeros++;
-                else ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
-        }
-        return dp[M][N];
-    }
-}
+// Last updated: 5/26/2026, 9:25:24 PM
+1class Solution {
+2    public int lastStoneWeightII(int[] stones) {
+3        int sum = 0;
+4        for(int num : stones){
+5            sum += num;
+6        }
+7        return helper(stones,sum/2,stones.length,sum);
+8    }
+9    public int helper(int[] stones,int sum,int n,int total){
+10        boolean[][] t = new boolean[n+1][sum+1];
+11        for(int i = 0;i<=n;i++){
+12            t[i][0] = true;
+13        }
+14       
+15        for(int i =1;i<=n;i++){
+16            for(int j =1;j<=sum;j++){
+17                if(stones[i-1]<=j){
+18                    t[i][j]=t[i-1][j-stones[i-1]] || t[i-1][j];
+19                }else{
+20                    t[i][j] = t[i-1][j];
+21                }
+22            }
+23        }
+24        int s1 = 0;
+25
+26        for(int j = sum; j >= 0; j--){
+27            if(t[stones.length][j]){
+28                s1 = j;
+29                break;
+30            }
+31        }
+32
+33        return total - 2*s1;
+34}}
