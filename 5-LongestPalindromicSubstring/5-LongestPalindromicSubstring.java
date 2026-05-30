4c4d09f539aaa9d9f4b9ea8e5d17225b8d4d8c50
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-05-30 14:45:27 +0530

    File updated at 5/30/2026, 2:45:26 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,31 +1,31 @@
-// Last updated: 10/13/2025, 11:32:35 PM
-class Solution {
-    public String longestPalindrome(String s) {
-        String g= "";
-         for (int left = 0; left < s.length(); left++) {
-            for (int right = left; right < s.length(); right++) {
-                String substring = s.substring(left, right + 1);
-
-                if (isPalindrome(substring) && substring.length() > g.length()) {
-                    g = substring;
-                }
-            }
-        }
-        return g;
-    }
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-
-}
+// Last updated: 5/30/2026, 2:45:26 PM
+1class Solution {
+2    public String longestPalindrome(String s) {
+3        String g= "";
+4         for (int left = 0; left < s.length(); left++) {
+5            for (int right = left; right < s.length(); right++) {
+6                String substring = s.substring(left, right + 1);
+7
+8                if (isPalindrome(substring) && substring.length() > g.length()) {
+9                    g = substring;
+10                }
+11            }
+12        }
+13        return g;
+14    }
+15    public static boolean isPalindrome(String str) {
+16        int left = 0;
+17        int right = str.length() - 1;
+18
+19        while (left < right) {
+20            if (str.charAt(left) != str.charAt(right)) {
+21                return false;
+22            }
+23            left++;
+24            right--;
+25        }
+26
+27        return true;
+28    }
+29
+30}
