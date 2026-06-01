0a4b7ea7d6bfa268c6753dba122fec2fcd637fbf
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-06-01 23:22:38 +0530

    File updated at 6/1/2026, 11:22:37 PM

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,27 +1,29 @@
-// Last updated: 6/1/2026, 5:24:19 PM
+// Last updated: 6/1/2026, 11:22:37 PM
 1class Solution {
-2    int[][] t = new int[201][201];
-3    public int minPathSum(int[][] grid) {
-4        int m = grid.length;
-5        for (int i = 0; i <= m; i++) {
-6    Arrays.fill(t[i], -1);
-7}
-8        int n  = grid[0].length;
-9        return solve(grid , m-1,n-1);
+2    int[][] t = new int[101][101];
+3    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+4        int m = obstacleGrid.length;
+5        for(int i = 0;i<m;i++){
+6            Arrays.fill(t[i],-1);
+7        }
+8        int n = obstacleGrid[0].length;
+9        return solve(obstacleGrid,m-1,n-1);
 10    }
-11    public int solve(int[][] grid , int m,int n ){
-12        if(n==0 && m == 0){
-13            return grid[0][0];
-14        } if (m < 0 || n < 0) {
-15            return Integer.MAX_VALUE;
-16        }
-17        if(t[m][n] != -1){
-18            return t[m][n];
-19        }
-20
-21        int left = solve(grid,m,n-1);
-22        int right = solve(grid,m-1,n);
-23        return t[m][n] = grid[m][n] + Math.min(left,right);
-24
-25    }
-26}
+11    public int solve(int[][] obstacleGrid,int m , int n){
+12        if(n<0 || m<0){
+13            return 0;
+14        }
+15        if(obstacleGrid[m][n] == 1){
+16            return 0;
+17        }
+18        if(n==0 && m==0){
+19            return 1;
+20        }
+21        if(t[m][n] != -1){
+22            return t[m][n];
+23        }
+24        int left  = obstacleGrid[m][n] + solve(obstacleGrid,m,n-1);
+25        int right  =  solve(obstacleGrid,m-1,n);
+26        return t[m][n] = left+right;
+27    }
+28}
