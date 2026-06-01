a571f5fb3ecc5dbd791a364c17a96e83ab616a5c
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-06-01 09:03:31 +0530

    File updated at 6/1/2026, 9:03:31 AM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,25 +1,17 @@
-// Last updated: 6/1/2026, 9:01:31 AM
-1class Solution {
-2    int[][] t = new int[101][101];
-3    public int uniquePaths(int m, int n) {
-4        for (int i = 0; i <= m; i++) {
-5    Arrays.fill(t[i], -1);
-6}
-7        return solve(m,n);
-8        
-9    }
-10    public int solve(int m, int n){
-11        if(m==1 || n==1){
-12            return 1;
-13        }
-14        if(m==0 || n==0){
-15            return 0;
-16        }
-17        if(t[m][n] != -1){
-18            return t[m][n];
-19        }
-20        int left =  solve(m,n-1);
-21        int right  = solve(m-1,n);
-22        return t[m][n] = left+right;
-23    }
-24}
+// Last updated: 6/1/2026, 9:03:31 AM
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return findPath(m-1,n-1,dp);
+    }
+    int findPath(int r, int c, int[][] dp){
+        if(r == 0 || c == 0){
+            return 1;
+        }
+        if(dp[r][c] != 0){
+            return dp[r][c];
+        }
+        dp[r][c] =  findPath(r-1,c,dp) + findPath(r,c-1,dp);
+        return dp[r][c];
+    }
+}
