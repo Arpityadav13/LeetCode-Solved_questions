73e65790d226c958620589e0b91b6636eb9c2b38
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-06-03 23:21:05 +0530

    Create 746-MinCostClimbingStairs

---
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+// Last updated: 6/3/2026, 11:21:04 PM
+1class Solution {
+2    int[] t;
+3    public int minCostClimbingStairs(int[] cost) {
+4        int n = cost.length;
+5        t = new int[n+1];
+6        Arrays.fill(t,-1);
+7        return solve(cost , n);
+8    }
+9    public int solve(int[] cost , int n){
+10        if(n==0 || n==1){
+11            return 0;
+12        }
+13        if(t[n] != -1){
+14            return t[n];
+15        }
+16        int left =  solve(cost , n-1) + cost[n-1];
+17        int right = solve(cost , n-2) + cost[n-2];
+18        return t[n] =  Math.min(left,right);
+19    }
+20}
