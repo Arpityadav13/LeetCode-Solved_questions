4a0850f04d4e9964075d4cc76943360cbe02ba60
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-06-07 09:25:55 +0530

    File updated at 6/7/2026, 9:25:53 AM

--- 162-FindPeakElement/162-FindPeakElement.java
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -1,18 +1,25 @@
-// Last updated: 10/13/2025, 11:31:42 PM
-class Solution {
-    public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left;        
-    }
-}
+// Last updated: 6/7/2026, 9:25:53 AM
+1class Solution {
+2    public int findPeakElement(int[] arr) {
+3        int n  =arr.length;
+4         if (n == 1) return 0;
+5        if (arr[0] > arr[1]) return 0;
+6        if (arr[n - 1] > arr[n - 2]) return n - 1;
+7        int i = 1 ;
+8        int j = arr.length-2;
+9        while(i<=j){
+10            int mid = i + (j-i)/2;
+11            if(arr[mid-1]< arr[mid] && arr[mid+1]< arr[mid]){
+12                return mid;
+13            }
+14            else if(arr[mid]<arr[mid+1]){
+15                i = mid+1;
+16
+17            }else{
+18                j = mid-1;
+19            }
+20
+21        }
+22        return -1;
+23    }
+24}
