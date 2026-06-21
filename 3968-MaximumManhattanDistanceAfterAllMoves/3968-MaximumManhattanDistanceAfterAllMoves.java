f737d82f06116a6128c8c5936944e3eb0b2b066a
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-06-21 14:50:53 +0530

    File updated at 6/21/2026, 2:50:53 PM

--- 3968-MaximumManhattanDistanceAfterAllMoves/3968-MaximumManhattanDistanceAfterAllMoves.java
+++ 3968-MaximumManhattanDistanceAfterAllMoves/3968-MaximumManhattanDistanceAfterAllMoves.java
@@ -1,27 +1,23 @@
-// Last updated: 6/21/2026, 2:30:09 PM
+// Last updated: 6/21/2026, 2:50:53 PM
 1class Solution {
-2    public int maxDistance(String moves) {
-3        int D = 0;
-4        int U = 0;
-5        int L = 0;
-6        int R = 0;
-7        int underscore = 0;
-8        for(int i  = 0;i<moves.length();i++){
-9            char c = moves.charAt(i);
-10            if(c == 'D'  ){
-11                D++;
-12            }else if(c == 'U'){
-13                 U++;
-14            }else if(c == 'L'){
-15                L++;
-16            }else if(c == 'R'){
-17                R++;
-18            }else{
-19                underscore++;
-20            }
-21        }
-22        int vertical = Math.abs(D-U);
-23        int horizontal = Math.abs(L-R);
-24        return vertical + horizontal + underscore;
-25    }
-26}
+2    public int countValidSubarrays(int[] nums, int x) {
+3        int count = 0;
+4        char ch = (char)(x + '0');
+5
+6        for (int i = 0; i < nums.length; i++) {
+7            long sum = 0;
+8
+9            for (int j = i; j < nums.length; j++) {
+10                sum += nums[j];
+11
+12                String s = Long.toString(sum);
+13
+14                if (s.charAt(0) == ch && s.charAt(s.length() - 1) == ch) {
+15                    count++;
+16                }
+17            }
+18        }
+19
+20        return count;
+21    }
+22}
