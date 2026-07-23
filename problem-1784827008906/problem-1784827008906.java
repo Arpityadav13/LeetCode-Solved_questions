91a9125d378652f8c9e53c42892d1baf955cd276
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-07-23 22:46:49 +0530

    Create

---
+++ problem-1784827008906/problem-1784827008906.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 10:46:48 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int list[] = new int[2];
+4        int i =0;
+5        while(i<nums.length){
+6            for(int n =i+1;n<nums.length;n++){
+7                if(nums[i]+nums[n]==target){
+8                    list[0]=i;
+9                    list[1]=n;
+10                }
+11            }
+12            i++;
+13        }
+14        return list;
+15    }
+16}
