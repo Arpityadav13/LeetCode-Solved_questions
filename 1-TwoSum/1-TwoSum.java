ce3a98e3aac1f600d10b6decffa75b77b0f182eb
Arpityadav13 <164322907+Arpityadav13@users.noreply.github.com>
2026-08-28 14:00:36 +0530

    File updated at 8/28/2026, 1:00:18 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,4 +1,4 @@
-// Last updated: 6/29/2026, 9:08:13 AM
+// Last updated: 8/28/2026, 1:00:18 PM
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
 3        int list[] = new int[2];
